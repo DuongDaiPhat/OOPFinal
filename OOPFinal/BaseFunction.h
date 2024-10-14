23b7981271d26296563ce2c13935060a1c71d359
DuongDaiPhat <duongphat521@gmail.com>
2024-10-14 10:33:42 +0700

    Complete Base Function

--- OOPFinal/BaseFunction.h
+++ OOPFinal/BaseFunction.h
@@ -1,1 +1,21 @@
-#pragma once
+﻿#pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
+#include <iostream>
+using namespace std;
+
+//Tạo biến con trỏ vào khung cửa sổ
+static SDL_Window *g_window;
+//Tạo biến con trỏ vào màn hình
+static SDL_Renderer* g_screen;
+//Tạo biến thu thập sự kiện người chơi
+static SDL_Event g_event;
+
+//Thông số cơ bản
+const int SCREEN_WIDTH = 1920;
+const int SCREEN_HEIGHT = 1280;
+
+//Hàm khởi tạo
+bool initGame(SDL_Window* window, SDL_Renderer* screen);
